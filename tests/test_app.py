764aa4b2002c0a4588ffc88066138001551ff410
Richard Blaauw <richard.blaauw@ogd.nl>
2021-10-10 16:18:00 +0200

    Add more tests to test the index and get_books responses

--- tests/test_app.py
+++ tests/test_app.py
@@ -11,5 +11,26 @@ def test_index(app, client):
     """
     res = client.get('/')
     assert res.status_code == 200
-    #expected = {'hello': 'world'}
-    #assert expected == json.loads(res.get_data(as_text=True))
+
+
+def test_index_content(app, client):
+    """
+    GIVEN a running Flask app
+    WHEN the client browses to /
+    THEN check for 'Shining' in the returned data
+    """
+    res = client.get('/')
+    assert b'<p>Title' in res.data
+    assert b'<p>Cover' in res.data
+    assert b'<p>Blurb' in res.data
+
+
+
+def test_get_books(app, client):
+    """
+    GIVEN a running Flask app
+    WHEN the client browses to /get_books
+    THEN check for status code 200
+    """
+    res = client.get('/get_books')
+    assert res.status_code == 200
